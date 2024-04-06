4f147e1af3c1678f674bfc7e9b4fe22900f3f59b
melekpro <melekpro6@gmail.com>
2024-04-05 19:13:43 -0700

    Update twrp_CPH2185.mk

--- twrp_CPH2185.mk
+++ twrp_CPH2185.mk
@@ -21,7 +21,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/twrp/config/common.mk)
+$(call inherit-product, vendor/pb/config/common.mk)
 
 PRODUCT_DEVICE := CPH2185
 PRODUCT_NAME := twrp_CPH2185
